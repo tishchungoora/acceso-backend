c6894574c2878f1b0f36561861c1e943c30de99c
Tish Chungoora <tishal82@gmail.com>
2020-01-08 14:03:38 +0000

    Created index methods for cards and categories; defined contents of the card and category serializer

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::CardsController < ApplicationController
+
+    def index
+        cards = Card.ApplicationController
+        render json: cards
+    end
+
 end
--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,3 +1,4 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :image_url
+  belongs_to :category
 end
