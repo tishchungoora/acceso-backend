8f8cff327bd047e1cb39e5db4d8c056266c6fb01
Tish Chungoora <tishal82@gmail.com>
2020-01-21 17:28:52 +0000

    Added get and post routes for dealing with user validation and login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
       resources :cards
       resources :categories
       resources :users, only: [:index, :create]
+      get '/validate_user', to: 'users#validate_user'
+      post '/login', to: 'users#login'
     end
   end
 end
